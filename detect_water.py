7340b38dfd2d9d46564da76e9e95be13e8246e87
zstone <zstonex@gmail.com>
2020-05-30 21:00:38 -0400

    individual sensors

--- detect_water.py
+++ detect_water.py
@@ -1,5 +1,4 @@
 import RPi.GPIO as GPIO
-from threading import Thread
 import time
 import paho.mqtt.client as mqtt
 leak_channel = 14
@@ -7,8 +6,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(leak_channel,GPIO.IN)
 client = mqtt.Client()
 
-leak_topic = "/brady/leak"
-ping_topic = "/brady/ping"
+leak_topic = "/home/brady/leak"
+status_topic = "home/brady/status"
+this_pi = "brady"
+this_pi_password = "tom"
 
 def on_connect(client,userdata,flags,rc):
     print ("connected with code "+str(rc))
@@ -16,27 +17,24 @@ def on_connect(client,userdata,flags,rc):
 def leak_update(channel) :
         if GPIO.input(channel):
             print("no water")
-            client.publish(leak_topic,"dry", 0)
+            client.publish(leak_topic, "dry", 1)
         else:
             print("water detected")
-            client.publish(leak_topic,"wet", 0)
+            client.publish(leak_topic, "wet", 1)
 
-def send_ping():
-  print("pinging:" + time.ctime())
-  client.publish(ping_topic,"alive",0)
-  time.sleep(30)
-  send_ping()
+def initialize_states() :
+    client.publish(status_topic, "online",1)
+    leak_update(leak_channel)
 
-client.username_pw_set("brady","tom")        
+client.username_pw_set(this_pi,this_pi_password)        
 client.on_connect=on_connect
+client.set_will(status_topic, payload = mk_payload("offline"))
 
 client.connect("homeassistant.local", 1883,60)
 
 
-
 GPIO.add_event_detect(leak_channel, GPIO.BOTH, bouncetime=300)
 GPIO.add_event_callback(leak_channel, leak_update)
-t = Thread(target=send_ping)
-t.start()
-leak_update(leak_channel)
+
+initialize_states()
 client.loop_forever()
